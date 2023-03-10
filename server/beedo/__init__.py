ef734f6a323f9012a20ff623a885d6122b14bb6c
Kay Cee <se.kaycee@gmail.com>
2023-03-10 22:21:35 +0000

    add static files

--- server/beedo/__init__.py
+++ server/beedo/__init__.py
@@ -1,31 +1,41 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
+from flask.json import JSONEncoder
+from flask_cors import CORS
+
+from bson import json_util, ObjectId
+from datetime import datetime, timedelta
+
+# from beedo.api.tasks import tasks_api_v1
+
+class MongoJsonEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return (obj.strftime("%Y-%m-%d %H:%M:%S"))
+        if isinstance(obj, ObjectId):
+            return (str(obj))
+        return (json_util.default(obj, json_util.CANONICAL_JSON_OPTIONS))
 
 
-def create_app(test_config=None):
-    '''Create and configure beedo package
-    '''
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    # don't forget to replace it with redis or mongodb
-    # app.config.from_mapping(
-    #     SECRET_KEY='dev',
-    #     DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    # )
+def create_app():
+    ''' Create and configure beedo package '''
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+    APP_DIR = os.path.abspath(os.path.dirname(__file__))
+    STATIC_FOLDER = os.path.join(APP_DIR, 'build/static')
+    TEMPLATE_FOLDER = os.path.join(APP_DIR, 'build')
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    app = Flask(__name__, static_folder=STATIC_FOLDER,
+                template_folder=TEMPLATE_FOLDER,
+                )
+    CORS(app)
+    app.json_encoder = MongoJsonEncoder
+    # app.register_blueprint(tasks_api_v1)
+
+    @app.route('/', defaults={'path': ''})
+    @app.route('/<path:path>')
+    def serve(path):
+        return (render_template('index.html'))
 
     # a simple page that says hello
     @app.route('/hello')
