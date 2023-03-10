f81cb3160d3a639204057c12456d0b121f3c2830
Kay Cee <se.kaycee@gmail.com>
2023-03-10 21:37:29 +0000

    recode dp.py

--- server/beedo/db.py
+++ server/beedo/db.py
@@ -1,4 +1,26 @@
-from mongoengine import connect
+import bson
 
-connect(host='mongodb+srv://kaycee:Kay72Cee\
-        @cluster0.s05wkfj.mongodb.net/?retryWrites=true&w=majority')
+from flask import current_app, g
+from werkzeug.local import LocalProxy
+from flask_pymongo import PyMongo
+
+from pymongo.errors import DuplicateKeyError, OperationFailure
+from bson.objectid import ObjectId
+from bson.errors import InvalidId
+
+
+def get_db():
+    """
+    Configuration method to return db instance
+    """
+    db = getattr(g, "_database", None)
+
+    if db is None:
+
+        db = g._database = PyMongo(current_app).db
+       
+    return (db)
+
+
+# Use LocalProxy to read the global db instance with just `db`
+db = LocalProxy(get_db)
